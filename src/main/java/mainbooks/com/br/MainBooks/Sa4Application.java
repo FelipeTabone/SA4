50fdd3b32fdb3c272f8d4d750cd8abb868319fa6
FelipeTabone <Felipea.tabone@gmail.com>
2023-08-28 14:20:40 -0300

    V-0.0.1

---
+++ src/main/java/mainbooks/com/br/MainBooks/Sa4Application.java
@@ -0,0 +1,13 @@
+package mainbooks.com.br.MainBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sa4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sa4Application.class, args);
+	}
+
+}
---
+++ src/test/java/mainbooks/com/br/MainBooks/Sa4ApplicationTests.java
@@ -0,0 +1,13 @@
+package mainbooks.com.br.MainBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
